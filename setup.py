45df2e7ed1aed220e11adf0da82e92e55df05547
Giulio Marescotti <Giulio.Marescotti.ext@ansaldo-sts.com>
2016-04-05 17:12:10 +0200

    Added Windows platform compatibility
    - Tested only for Windows7 + VisualStudio 12

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
 from distutils.core import setup, Extension
+import platform
+
+if platform.system() == 'Windows':
+    windows_extensions=dict(
+        define_macros=[('WIN32', None), ('ESL_EXPORTS', None)],
+        libraries=['Ws2_32']
+    )
+    import os
+    if 'VS90COMNTOOLS' not in os.environ:
+        # Try out different version of Visual Studio
+        # The only version seen working is Windows7 + VisualStudio 12.0
+        for VS in ['120', '110', '100', '130', '140']:
+            if 'VS%sCOMNTOOLS' % VS in os.environ:
+                os.environ['VS90COMNTOOLS'] = os.environ['VS%sCOMNTOOLS' % VS]
+                break
+    assert 'VS90COMNTOOLS' in os.environ, "Not found any Visual Studio Installation!"
+else:
+    windows_extensions = dict()
 
 setup (name = 'FreeSWITCH-esl-python',
        version = '0.1vdev',
@@ -10,8 +28,7 @@ setup (name = 'FreeSWITCH-esl-python',
                                               'src/esl_config.c',
                                               'src/esl_oop.cpp',
                                               'src/esl_buffer.c'],
-       include_dirs=['include/'])],
+       include_dirs=['include/'], **windows_extensions)],
        packages = ['freeswitchESL'],
        pymodules = ['ESL'],
        description = 'Auto-generated swig python module for FreeSWITCH mod_esl with a binary component.',)
-	   
